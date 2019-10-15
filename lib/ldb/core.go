c43ed831e2564e923258e73cfa760f5d48c17f11
deeper-x <albertodeprezzo@gmail.com>
2019-10-15 18:52:47 +0200

    updated ldb core

--- lib/ldb/core.go
+++ lib/ldb/core.go
@@ -4,6 +4,12 @@ import (
 	"database/sql"
 	"log"
 
+	"os"
+
+	"github.com/gchaincl/dotsql"
+	"github.com/joho/godotenv"
+
+	// postgres lib used in connection
 	_ "github.com/lib/pq"
 )
 
@@ -11,11 +17,21 @@ type connection struct {
 	dsn string
 }
 
+type dotMapper struct {
+	resource *dotsql.DotSql
+}
+
+var mapper = &dotMapper{}
 var objConn connection
 
-//Connect to db
+// Connect to db
 func Connect() *sql.DB {
-	objConn.dsn = "postgres://<USER>:<PASSWORD>@127.0.0.1/<DBNAME>"
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
+	objConn.dsn = os.Getenv("DB_DSN")
+
 	db, err := sql.Open("postgres", objConn.dsn)
 
 	if err != nil {
@@ -23,4 +39,15 @@ func Connect() *sql.DB {
 	}
 
 	return db
+}
+
+// GenResource todo doc
+func (d *dotMapper) GenResource(qpath string) {
+	var dot, err = dotsql.LoadFromFile(qpath)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	d.resource = dot
 }
