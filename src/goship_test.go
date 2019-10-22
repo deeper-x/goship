59a1f08ac8db533a6655cf24193f3cab2e2ec170
deeper-x <albertodeprezzo@gmail.com>
2019-10-22 18:53:44 +0200

    integration tests

--- src/goship_test.go
+++ src/goship_test.go
@@ -15,16 +15,25 @@ func TestMain(t *testing.T) {
 	e := httptest.New(t, app)
 
 	e.GET("/").Expect().Status(httptest.StatusOK)
+	e.GET("/moored/28").Expect().Status(httptest.StatusOK)
 	e.GET("/anchored/28").Expect().Status(httptest.StatusOK)
 	e.GET(("/arrivalPrevisions/28")).Expect().Status(httptest.StatusOK)
 	e.GET(("/departurePrevisions/28")).Expect().Status(httptest.StatusOK)
 	e.GET(("/shiftingPrevisions/28")).Expect().Status(httptest.StatusOK)
 	e.GET(("/arrivalsToday/28")).Expect().Status(httptest.StatusOK)
 	e.GET(("/departuresToday/28")).Expect().Status(httptest.StatusOK)
+	e.GET("/shippedGoodsToday/28").Expect().Status(httptest.StatusOK)
+	e.GET("/trafficListToday/28").Expect().Status(httptest.StatusOK)
+	e.GET("/shiftingsToday/28").Expect().Status(httptest.StatusOK)
 
-	e.GET(("/arrivalsRegister/28/2019-01-01 00:00/2019-01-01 00:00/")).Expect().Status(httptest.StatusOK)
-	e.GET(("/departuresRegister/28/2019-01-01 00:00/2019-01-01 00:00/")).Expect().Status(httptest.StatusOK)
-	e.GET(("/roadsteadRegister/28/2019-01-01 00:00/2019-01-01 00:00/")).Expect().Status(httptest.StatusOK)
-	e.GET(("/mooredRegister/28/2019-01-01 00:00/2019-01-01 00:00/")).Expect().Status(httptest.StatusOK)
+	e.GET("/arrivalsRegister/28/2019-01-01 00:00/2019-01-01 00:00/").Expect().Status(httptest.StatusOK)
+	e.GET("/departuresRegister/28/2019-01-01 00:00/2019-01-01 00:00/").Expect().Status(httptest.StatusOK)
+	e.GET("/roadsteadRegister/28/2019-01-01 00:00/2019-01-01 00:00/").Expect().Status(httptest.StatusOK)
+	e.GET("/mooredRegister/28/2019-01-01 00:00/2019-01-01 00:00/").Expect().Status(httptest.StatusOK)
+	e.GET("/shiftingsRegister/28/2019-10-02 00:00/2019-10-06 23:59").Expect().Status(httptest.StatusOK)
+	e.GET("/shippedGoodsRegister/28/2019-10-02 00:00/2019-10-06 23:59").Expect().Status(httptest.StatusOK)
+	e.GET("/trafficListRegister/28/2019-10-02 00:00/2019-10-06 23:59").Expect().Status(httptest.StatusOK)
+
+	e.GET("/weatherActiveStations").Expect().Status(httptest.StatusOK)
 
 }
