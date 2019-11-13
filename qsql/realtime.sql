106d934b0b9b4cc4b94d8910b92ef0e777a5abaa
deeper-x <albertodeprezzo@gmail.com>
2019-11-13 14:35:14 +0100

    fix traffic list query

--- qsql/realtime.sql
+++ qsql/realtime.sql
@@ -457,16 +457,16 @@ num_minibus,
 traffic_list_mvnt_type, 
 traffic_list_categories.description,
 quays.description AS quay
-FROM traffic_list INNER JOIN control_unit_data
+FROM traffic_list LEFT JOIN control_unit_data
 ON fk_control_unit_data = id_control_unit_data
-INNER JOIN traffic_list_categories
+LEFT JOIN traffic_list_categories
 ON fk_traffic_list_category = id_traffic_list_category
-INNER JOIN ships
+RIGHT JOIN ships
 ON control_unit_data.fk_ship = id_ship
-INNER JOIN maneuverings
+LEFT JOIN maneuverings
 ON maneuverings.fk_control_unit_data = control_unit_data.id_control_unit_data
 AND maneuverings.fk_state = 17
-INNER JOIN quays
+LEFT JOIN quays
 ON maneuverings.fk_stop_quay = quays.id_quay
 WHERE control_unit_data.fk_portinformer = $1
 AND control_unit_data.is_active = true;
